5c2f83f6911855ab6ff8a2dc6ca291fa3e4397c6
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2026-06-19 08:00:49 +0100

    port to Python 3.13

--- old.py
+++ old.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Use the proper idiom in the main module ...
-# NOTE: See https://docs.python.org/3.12/library/multiprocessing.html#the-spawn-and-forkserver-start-methods
+# NOTE: See https://docs.python.org/3.13/library/multiprocessing.html#the-spawn-and-forkserver-start-methods
 if __name__ == "__main__":
     # Import standard modules ...
     import argparse
@@ -154,7 +154,7 @@ if __name__ == "__main__":
                     print(f"    Loading \"{jName}\" ...")
 
                     # Load GeoJSON file ...
-                    with open(jName, "rt", encoding = "utf-8") as fObj:
+                    with open(jName, mode = "rt", encoding = "utf-8") as fObj:
                         savedCircle = shapely.geometry.shape(geojson.load(fObj))
 
                     # Plot saved great circle ...
